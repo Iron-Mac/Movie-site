ec12de6300edbe1e99379406afa087497bba6448
Hamid <aliahmadihacker@gmail.com>
2021-10-05 15:11:08 +0330

    building UI a little bit, make important files seprated , solve some rtl problems

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
+import os
+import fastapi_jinja
 import uvicorn
+from starlette.staticfiles import StaticFiles
+from views import home,movie,tvshow,account,plan
+
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {'message': 'Hello World'}
+
+
+dev_mode = True
+
+folder = os.path.dirname(__file__)
+template_folder = os.path.join(folder, 'templates')
+template_folder = os.path.abspath(template_folder)
+
+fastapi_jinja.global_init(template_folder, auto_reload=dev_mode)
+
+
+app.mount('/static',StaticFiles(directory = 'static'),name='static')
+
+app.include_router(home.router)
+app.include_router(movie.router)
+app.include_router(tvshow.router)
+app.include_router(account.router)
+app.include_router(plan.router)
 
 if __name__ == '__main__':
     uvicorn.run(app)
